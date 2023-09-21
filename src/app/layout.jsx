587a1f5f70fb570e6de565fdf68f9a3e5e32827a
BeiGod <saidsamuel2006@hotmail.com>
2023-09-20 19:17:27 -0500

    cambio de fetchcars a los layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,7 @@
-'use client'
-
 import './globals.css'
 import 'animate.css'
 import { Inter } from 'next/font/google'
 import Layout from '@/layouts/Layout'
-import useCarsStore from '@/hooks/useCarsStore'
-import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,12 +11,6 @@ export const metadata = {
 }
 
 export default function RootLayout ({ children }) {
-  const { fetchCars } = useCarsStore()
-  // al iniciar la app, se cargan los autos en el store
-  useEffect(() => {
-    fetchCars()
-  }, [])
-
   return (
     <html lang='en'>
       <body className={inter.className}>
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -5,8 +5,16 @@ import NoNavigationLayout from './NoNavigationLayout'
 import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
 import PanelLayout from './PanelLayout'
+import useCarsStore from '@/hooks/useCarsStore'
+import { useEffect } from 'react'
 
 export default function Layout ({ children }) {
+  const { fetchCars } = useCarsStore()
+  // al iniciar la app, se cargan los autos en el store
+  useEffect(() => {
+    fetchCars()
+  }, [])
+
   const path = usePathname()
 
   if (['/login'].includes(path)) {

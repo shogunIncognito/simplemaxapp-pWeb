c6dc7628229fccaffee924ab1563f51146b729bc
EmersonR-dev <emerson.roncancio@uniminuto.edu.co>
2023-10-04 08:06:15 -0500

    carrousel final

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { brandsCarrousel } from '@/helpers/inputs'
+import { brandsCarrousel } from '@/helpers/data'
 
 export default function Carrusel () {
   return (

70475338350097f1659ab1d3c58b1953f2ee79e6
BeiGod <saidsamuel2006@hotmail.com>
2023-11-04 18:04:45 -0500

    Add asterisk to labels in CarForm component

--- src/components/panel/CarForm.jsx
+++ src/components/panel/CarForm.jsx
@@ -25,7 +25,7 @@ export default function CarForm ({
       <form onSubmit={handleSubmit} className='overflow-auto lg:max-h-90[dvh] max-h-[80dvh]'>
         <div className='grid grid-cols-2 md:grid-cols-3 gap-3'>
           <div className='flex flex-col gap-1 overflow-ellipsis'>
-            <label className='dark:text-white text-black whitespace-nowrap text-ellipsis overflow-hidden'>Marca</label>
+            <label className='dark:text-white after:content-["*"] text-black whitespace-nowrap text-ellipsis overflow-hidden'>Marca</label>
             <Select onChange={handleChange} value={values.brandId} name='brandId' id='brandId'>
               <option className='bg-slate-100 text-black dark:text-white dark:bg-slate-700' value='' disabled>Seleccione una marca</option>
               {brands.map(brand => (
@@ -35,7 +35,7 @@ export default function CarForm ({
           </div>
 
           <div className='flex flex-col gap-1'>
-            <label className='dark:text-white text-black whitespace-nowrap text-ellipsis overflow-hidden'>Tipo combustible</label>
+            <label className='dark:text-white after:content-["*"] text-black whitespace-nowrap text-ellipsis overflow-hidden'>Tipo combustible</label>
             <Select onChange={handleChange} value={values.fuel} name='fuel' id='fuel'>
               <option className='bg-slate-100 text-black dark:text-white dark:bg-slate-700' value='corriente'>Corriente</option>
               <option className='bg-slate-100 text-black dark:text-white dark:bg-slate-700' value='diesel'>Diesel</option>
@@ -43,7 +43,7 @@ export default function CarForm ({
           </div>
 
           <div className='flex flex-col gap-1 overflow-ellipsis'>
-            <label className='dark:text-white text-black whitespace-nowrap text-ellipsis overflow-hidden'>Transmisión</label>
+            <label className='dark:text-white after:content-["*"] text-black whitespace-nowrap text-ellipsis overflow-hidden'>Transmisión</label>
             <Select onChange={handleChange} value={values.transmission} name='transmission' id='transmission'>
               <option className='bg-slate-100 text-black dark:text-white dark:bg-slate-700' value='manual'>Manual</option>
               <option className='bg-slate-100 text-black dark:text-white dark:bg-slate-700' value='automatica'>Automática</option>
@@ -51,7 +51,7 @@ export default function CarForm ({
           </div>
 
           <div className='flex flex-col gap-1 overflow-ellipsis'>
-            <label className='dark:text-white text-black whitespace-nowrap text-ellipsis overflow-hidden'>Tipo de vehículo</label>
+            <label className='dark:text-white after:content-["*"] text-black whitespace-nowrap text-ellipsis overflow-hidden'>Tipo de vehículo</label>
             <Select onChange={handleChange} value={values.type} name='type' id='type'>
               <option className='bg-slate-100 text-black dark:text-white dark:bg-slate-700' value='automovil'>Automóvil</option>
               <option className='bg-slate-100 text-black dark:text-white dark:bg-slate-700' value='camioneta'>Camioneta</option>
@@ -59,7 +59,7 @@ export default function CarForm ({
           </div>
 
           <div className='flex flex-col gap-1 overflow-ellipsis'>
-            <label className='dark:text-white text-black whitespace-nowrap text-ellipsis overflow-hidden'>CC</label>
+            <label className='dark:text-white after:content-["*"] text-black whitespace-nowrap text-ellipsis overflow-hidden'>CC</label>
             <Select onChange={handleChange} value={values.cc} name='cc' id='cc'>
 
               {selectOptionsCC.map((cc, index) => (
@@ -71,12 +71,16 @@ export default function CarForm ({
           {
             carInputs.map((input, index) => (
               <div key={index} className='flex flex-col gap-1'>
-                <label className='dark:text-white text-black whitespace-nowrap text-ellipsis overflow-hidden'>{input.label}</label>
+                <label
+                  className={`dark:text-white text-black whitespace-nowrap text-ellipsis overflow-hidden ${input.name !== 'description' ? 'after:content-["*"]' : ''}`}
+                >
+                  {input.label}
+                </label>
                 <Input
                   onChange={handleChange}
                   value={values[input.name]}
                   required={input.name !== 'description'}
-                  className='p-2'
+                  className='p-2 '
                   type={input.type}
                   name={input.name}
                   placeholder={input.placeholder}
